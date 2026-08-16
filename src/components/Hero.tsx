869ef4a316abca428065e6c480bdea2b02b1dc47
Anson Jia <ansonjia123@gmail.com>
2026-08-15 20:56:13 -0400

    added hero component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,23 @@
+function Hero() {
+    return (
+      <section className="hero">
+        <div className="hero-content">
+          <p className="hero-label">Anson Jia</p>
+  
+          <h1>
+            Interactive
+            <br />
+            Software
+            <br />
+            Developer
+          </h1>
+  
+          <p className="hero-description">
+            From ideas to design, from design to experience.
+          </p>
+        </div>
+      </section>
+    )
+  }
+  
+  export default Hero
