d86996afb098e9b898d3e0b88131c686a95b0049
Anson Jia <ansonjia123@gmail.com>
2026-08-18 08:09:07 -0400

    added shaders

---
+++ src/shaders/shaderHelpers.ts
@@ -0,0 +1,40 @@
+export const createShader = (//compile shader
+    gl: WebGLRenderingContext,
+    type: number,
+    source: string
+  ) => {
+    const shader = gl.createShader(type)!;
+  
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+  
+    if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      return shader;
+    }
+  
+    console.error(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+  
+    return null;
+  };
+  
+  export const createProgram = (//link shaders into a program
+    gl: WebGLRenderingContext,
+    vertexShader: WebGLShader,
+    fragmentShader: WebGLShader
+  ) => {
+    const program = gl.createProgram()!;
+  
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+  
+    if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
+      return program;
+    }
+  
+    console.error(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+  
+    return null;
+  };
