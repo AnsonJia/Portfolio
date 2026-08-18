f3d7b06c67ceb7392d5f9ae59857d3b266ebf513
Anson Jia <ansonjia123@gmail.com>
2026-08-18 18:44:12 -0400

    updated config to supress deprecation warnings

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  css: {//suppress deprecation warnings
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: [
+          'import',
+          'color-functions',
+          'global-builtin',
+          'if-function',
+        ],
+      },
+    },
+  },
 })
