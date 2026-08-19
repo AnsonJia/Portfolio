429fa69c73eed9c68ca82f290ca8aa75ba015b66
Anson Jia <ansonjia123@gmail.com>
2026-08-19 08:52:30 -0400

    bottom bar

---
+++ src/components/BottomBar/BottomBar.tsx
@@ -0,0 +1,53 @@
+//import MWIcon from './MWIcon';
+//import StatusIndicator from './StatusIndicator';
+import './BottomBar.scss';
+
+export default function BottomBar() {
+  return (
+    <div className="bottom-bar row pb-2 pb-md-3 align-items-end d-flex justify-content-between">  {/*bottom bar container bootstrap padding*/}
+      <div className="col-auto d-flex align-items-center"> {/*bottom bar left side*/}
+        {/*<MWIcon size={45} type="notext" />*/}
+
+        <h1 className="bottom-navigation__title ms-4">
+          <span>Anson Jia</span>
+          <br />
+          <span>Interactive Software Developer</span>
+        </h1>
+      </div>
+
+      <div className="col-auto mb-3 d-flex align-items-center"> {/*bottom bar right side*/}
+      <a
+        href="mailto:aj5351@nyu.edu"
+        className="me-4 text-decoration-none"
+      >
+        <i className="fa-solid fa-envelope fa-xl email-icon mouse-sm d-none d-xl-inline" />
+      </a>
+        {/*<a
+          //href=""
+          target="_blank"
+          className="me-4 text-decoration-none"
+        >
+          <i className="fa-brands fa-x-twitter fa-xl twitter-icon mouse-sm d-none d-xl-inline" />
+        </a>*/}
+
+        <a
+          href="https://github.com/AnsonJia"
+          target="_blank"
+          className="me-4 text-decoration-none"
+        >
+          <i className="fa-brands fa-github fa-xl github-icon mouse-sm d-none d-xl-inline" />
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/anson-jia-043135342"
+          target="_blank"
+          className="me-5 text-decoration-none"
+        >
+          <i className="fa-brands fa-linkedin fa-xl linkedin-icon mouse-sm d-none d-xl-inline" />
+        </a>
+
+        {/*<StatusIndicator type="light" />*/}
+      </div>
+    </div>
+  );
+}
