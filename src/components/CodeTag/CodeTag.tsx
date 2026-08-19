a3279a346a11cc80ccf77320185fd2668c76765f
Anson Jia <ansonjia123@gmail.com>
2026-08-18 23:09:53 -0400

    moved to folder

---
+++ src/components/CodeTag/CodeTag.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function CodeTag({//displau a tag takes 2 props
+  children,
+  className = '',
+}: {
+  children: React.ReactNode;//generic
+  className?: string;//optional
+}) {
+  return (//format
+    <div className={`code-tag ${className}`}>
+      &lt; {children} /&gt;
+    </div>
+  );
+}
