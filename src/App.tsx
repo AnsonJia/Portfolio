921baad6de46578a5d753ff1732d9548245f66d5
Anson Jia <ansonjia123@gmail.com>
2026-08-18 20:18:43 -0400

    updated app with default layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+//import { Routes, Route } from 'react-router-dom';
+import Hero from './components/Hero.tsx'
+import DefaultLayout from './layouts/DefaultLayout.tsx';
 import Home from './pages/Home.tsx'
 
-function App() {
-  return <Home />
+export default function App() {
+  return (
+    <DefaultLayout>
+      <Hero />
+    </DefaultLayout>
+  );
 }
-
-export default App
