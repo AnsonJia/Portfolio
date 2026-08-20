6e24a81a78580e6f6309a9e332ea7ab32fa31b22
Anson Jia <ansonjia123@gmail.com>
2026-08-20 13:02:06 -0400

    added route to home about and contact

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-//import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Hero from './components/Hero/Hero.tsx'
+import Cursor from './components/Cursor/Cursor.tsx';
 import DefaultLayout from './layouts/DefaultLayout.tsx';
-//import Home from './pages/Home.tsx'
+import Home from './pages/Home.tsx';
+import About from './pages/About/About.tsx';
+import Contact from './pages/Contact/Contact.tsx';
+
 
 export default function App() {
   return (
     <DefaultLayout>
       <Hero />
+      <Cursor />
+      
+      <Routes> 
+        <Route path="/" element={<Home />} />
+        
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        
+      </Routes> 
     </DefaultLayout>
   );
 }
