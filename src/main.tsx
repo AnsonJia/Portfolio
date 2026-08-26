201d262fca1cb2f531eb17e7a3db43521965651e
Anson Jia <ansonjia123@gmail.com>
2026-08-26 09:12:53 -0400

    import fontawsome css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import '@fortawesome/fontawesome-free/css/all.min.css';//fontawsome 7
+import 'font-awesome/css/font-awesome.min.css';//fontawsome 4
 
 import './styles/app.scss';
 
