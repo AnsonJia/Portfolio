1c185d1fac7465b7b42a5e0a1c80ca509ef4e35a
Anson Jia <ansonjia123@gmail.com>
2026-08-28 10:45:26 -0400

    updated about me info

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -19,7 +19,7 @@ export default function About() {
       <div className="container-xxxl">
         <div className="row mt-0 mt-lg-6">
           <main className="d-flex flex-column">
-            <div className="col-12 col-md-8 col-lg-7 col-xl-6 col-xxl-5 mb-4">
+            <div className="col-12 col-md-8 col-lg-7 col-xl-6 col-xxl-5 mb-0">
               <Link
                 className="back-link mouse-md mt-4 mt-lg-0"
                 to="/"
@@ -41,33 +41,38 @@ export default function About() {
               </div>
 
               <h2 className="my-4">
-                Hey I'm Anson — an interactive developer.
+                Hey I'm Anson — An Interactive Software Developer.
               </h2>
 
               <div>
                 <p>
-                  paragraph 1
+                  I'm a Computer Science graduate and software developer focused on building modern web applications.
                 </p>
 
                 <p>
-                  pargraph 2
+                  I like working across the stack, building interfaces, designing APIs, working with data, and figuring out the problems that inevitably come along the way. 
+                  Over the past few years, I've had the opportunity to work on real-world applications using React, Java, Spring Boot, and SQL.
                 </p>
 
                 <p>
-                  paragraph 3
+                  I'm currently continuing my Computer Science journey at NYU and seeing where that takes me.
                 </p>
               </div>
+            </div>
+
+            <div className='col-12 col-md-8 col-lg-7 col-xl-6 col-xxl-5 mb-0'>
+              <hr className="mb-0" />
             </div>
 
             <div className="col-12 col-md-8 col-lg-7 col-xl-6 col-xxl-5 mb-4 mb-lg-8">
               <h3 className="mt-5 mb-4">Availability</h3>
 
               <p>
-                idk
+                I'm always open to connecting with people, discussing interesting ideas, and exploring new opportunities.
               </p>
 
               <p className="mb-5">
-                xdddd
+                If you have a project in mind, want to collaborate, or simply want to get in touch, feel free to reach out.
               </p>
 
               <ActionButton
@@ -79,6 +84,7 @@ export default function About() {
                 Let's chat
               </ActionButton>
             </div>
+
           </main>
         </div>
       </div>
