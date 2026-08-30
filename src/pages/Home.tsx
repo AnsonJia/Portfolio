6e4f9e0a92aed6abc116e05216b793b21017ca0a
Anson Jia <ansonjia123@gmail.com>
2026-08-30 19:24:20 -0400

    updated home page to include scrollhint to nav to explore page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,30 @@
+import { useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function Home() {
+import ScrollHint from '../components/ScrollHint/ScrollHint';
+
+export default function Home() {
+  const navigate = useNavigate();
+  const navigating = useRef(false);//track nav
+
+  useEffect(() => {
+    const handleWheel = (e: WheelEvent) => {
+      if (e.deltaY > 0 && !navigating.current) {//check is scroll down and havent started nav yet
+        navigating.current = true;//currently navigating to prevent repeated nav
+        navigate('/explore');//navigate to the explore page on scroll down
+      }
+    };
+
+    window.addEventListener('wheel', handleWheel);//listener for scrollwheel
+
+    return () => {//cleanup
+      window.removeEventListener('wheel', handleWheel);
+    };
+  }, [navigate]);//run effect if navigate changes
+  
   return (
     <section className="page">
+      <ScrollHint />
     </section>
-  )
+  );
 }
-
-export default Home
