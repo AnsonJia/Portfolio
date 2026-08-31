fa1a5a0191cdfcc04ff4b9944670e6120bbac9a1
Anson Jia <ansonjia123@gmail.com>
2026-08-30 23:17:52 -0400

    temporary explore placeholder

---
+++ src/pages/Explore/Explore.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Explore.scss';
+
+export default function Explore() {
+    const navigate = useNavigate();
+    const navigating = useRef(false);
+  
+    useEffect(() => {
+      const handleWheel = (e: WheelEvent) => {
+        if (e.deltaY < 0 && !navigating.current) {
+          navigating.current = true;
+          navigate('/');
+        }
+      };
+  
+      window.addEventListener('wheel', handleWheel);
+  
+      return () => {
+        window.removeEventListener('wheel', handleWheel);
+      };
+    }, [navigate]);
+
+    //other things here
+
+
+
+
+  
+    return (
+      <section className="page">
+        <main className="explore-page">
+        <div className="explore-content">
+            <p className="explore-label">Explore</p>
+
+            <h1>
+            My Projects
+            </h1>
+
+            <p>
+            A collection of projects I've worked on across software development,
+            web applications, and other technologies.
+            </p>
+            
+        </div>
+        </main>
+      </section>
+    );
+  }
